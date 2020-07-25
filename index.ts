78a2cd50f4c2ad450c6dcfb21cc8d5c58f7c5f2d
Marc Rousavy <marcrousavy@hotmail.com>
2020-07-25 14:26:02 +0200

    Add JS funcs

--- index.ts
+++ index.ts
@@ -1,5 +1,27 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { NotificationBadge } = NativeModules;
 
-export default NotificationBadge;
+export function setBadgeCount(badgeCount: number): void {
+  if (Platform.OS === 'ios') {
+    NotificationBadge.setBadgeCount(badgeCount);
+  } else {
+    throw new Error(`setBadgeCount is not supported on ${Platform.OS}!`)
+  }
+}
+
+export function getBadgeCount(): Promise<number> {
+  if (Platform.OS === 'ios') {
+    return NotificationBadge.getBadgeCount();
+  } else {
+    throw new Error(`getBadgeCount is not supported on ${Platform.OS}!`)
+  }
+}
+
+export function getBadgeCountSync(): number {
+  if (Platform.OS === 'ios') {
+    return NotificationBadge.getBadgeCountSync();
+  } else {
+    throw new Error(`getBadgeCountSync is not supported on ${Platform.OS}!`)
+  }
+}
