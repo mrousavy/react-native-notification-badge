f2d72c4d99b6acf5232364ab3230e9b0de59e8af
Marc Rousavy <marcrousavy@hotmail.com>
2020-07-25 14:41:59 +0200

    Run on main thread

--- ios/NotificationBadge.swift
+++ ios/NotificationBadge.swift
@@ -9,21 +9,33 @@
 import Foundation
 import UIKit
 
-@objc(NearbyMessages)
+@objc(NotificationBadge)
 class NotificationBadge: NSObject {
 	@objc(setBadgeCount:)
 	func setBadgeCount(_ badgeCount: NSNumber) -> Void {
-		UIApplication.shared.applicationIconBadgeNumber = badgeCount.intValue
+		DispatchQueue.main.async {
+			UIApplication.shared.applicationIconBadgeNumber = badgeCount.intValue
+		}
 	}
 
 	@objc(getBadgeCount:rejecter:)
-	func getBadgeCount(_ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-		let badgeCount = NSNumber(value: UIApplication.shared.applicationIconBadgeNumber)
-		resolve(badgeCount)
+	func getBadgeCount(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
+		DispatchQueue.main.async {
+			let badgeCount = NSNumber(value: UIApplication.shared.applicationIconBadgeNumber)
+			resolve(badgeCount)
+		}
 	}
 
 	@objc
 	func getBadgeCountSync() -> NSNumber {
+		if !Thread.isMainThread {
+			NSLog("Error! getBadgeCountSync() has to be called from the main Thread!")
+		}
 		return NSNumber(value: UIApplication.shared.applicationIconBadgeNumber)
+	}
+	
+	@objc
+	static func requiresMainQueueSetup() -> Bool {
+		return true
 	}
 }
